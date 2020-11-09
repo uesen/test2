39d25ef781f1a4cfe15849beacd12373d774e830
uesen <redlist.text@gmail.com>
2020-11-09 19:02:10 +0900

    port

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var http = require('http');
 var socketio = require('socket.io')(http);//なんかHTTPたした
 var fs = require('fs');
+
+
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type' : 'text/html'});
     res.end(fs.readFileSync(__dirname + '/socket.html', 'utf-8'));
-}).listen(3000);  // ポート競合の場合は値を変更
+}).listen(process.env.PORT || 3000);  // ポート競合の場合は値を変更
  
 
 var io = socketio.listen(server);
