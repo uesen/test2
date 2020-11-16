d5e1413e14b9307cf32a15b82f99d250ae153607
uesen <redlist.text@gmail.com>
2020-11-17 06:19:12 +0900

    io

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.get('/' , function(req, res){
     res.sendFile(__dirname+'/indexpmae.html');
 });
 
-io.sockets.on('connection',function(socket){
+io.on('connection',function(socket){
     connectNum++;
     
     
